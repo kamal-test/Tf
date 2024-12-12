b3a67f705052677a4e86a442ab7b8652a754d811
akshatsinha007 <156403098+akshatsinha007@users.noreply.github.com>
2024-12-12 13:34:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-west-2"  # Replace with your desired region
+  region     = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # Create a VPC
