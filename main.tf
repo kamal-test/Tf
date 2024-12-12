100dff77c41465edf0aa0703824ed3e2af0b07cc
akshatsinha007 <156403098+akshatsinha007@users.noreply.github.com>
2024-12-12 13:42:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
 # Configure the AWS Provider
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
+# Configure AWS Provider with declared variables
 provider "aws" {
   region     = "us-west-2"
   access_key = var.aws_access_key
